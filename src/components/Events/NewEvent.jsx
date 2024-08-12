af1a5b92dc081eab3bc5d91723c97ee9507ab05a
Kiet Cao <kietcao@tuta.io>
2024-08-12 22:07:40 +0900

    use useMutation to add new event

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,25 +1,43 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { useMutation } from "@tanstack/react-query";
+import { createNewEvent } from "../../utils/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const { isPending, isError, error, mutate } = useMutation({
+    mutationFn: createNewEvent,
+  });
+
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && <p>Submitting...</p>}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="An error occurred"
+          message={error.info?.message ?? "Failed to fetch events"}
+        />
+      )}
     </Modal>
   );
 }
--- src/utils/http.js
+++ src/utils/http.js
@@ -17,3 +17,24 @@ export async function fetchEvents({ keyword, signal }) {
 
   return events;
 }
+
+export async function createNewEvent(eventData) {
+  const response = await fetch("http://localhost:3000/events", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while creating the events");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { events } = await response.json();
+
+  return events;
+}
