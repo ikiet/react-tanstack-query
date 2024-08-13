1a00e4cdd998559c169278ea105fe331eb5e3bfe
Kiet Cao <kietcao@tuta.io>
2024-08-13 10:05:38 +0900

    add fetch event detail & delete event logic

--- src/utils/http.js
+++ src/utils/http.js
@@ -59,3 +59,34 @@ export async function fetchSeletableImages({ signal }) {
 
   return images;
 }
+
+export async function fetchEvent({signal, id}) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    signal,
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetch the event");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+export async function deleteEvent({signal, id}) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    signal,
+    method: 'delete',
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while delete the event");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+}
