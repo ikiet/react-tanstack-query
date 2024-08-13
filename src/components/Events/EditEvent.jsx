add5b612c78da0b18b3b12e2ed505edb1f5f6728
Kiet Cao <kietcao@tuta.io>
2024-08-13 12:49:07 +0900

    fetch event data to edit form

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,27 +1,57 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from "react-router-dom";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { fetchEvent } from "../../utils/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import { useQuery } from "@tanstack/react-query";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { id } = useParams();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", id],
+    queryFn: ({ signal, queryKey }) => fetchEvent({ signal, id: queryKey[1] }),
+  });
 
   function handleSubmit(formData) {}
 
   function handleClose() {
-    navigate('../');
+    navigate("../");
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  let content;
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message ?? "Failed to fetch event"}
+      />
+    );
+  }
+  if (data) {
+    content = (
+      <>
         <Link to="../" className="button-text">
           Cancel
         </Link>
         <button type="submit" className="button">
           Update
         </button>
-      </EventForm>
+      </>
+    );
+  }
+
+  return (
+    <Modal onClose={handleClose}>
+      <EventForm inputData={data} onSubmit={handleSubmit}></EventForm>
+      {content}
     </Modal>
   );
 }
